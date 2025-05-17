a4dd39ac0afe5ef13dd57591313309bfe5833500
Brian Flores <brian.flores.ch@gmail.com>
2025-05-16 23:14:50 -0300

    Cambia estructura de carpetas y servidor para API Rest

---
+++ server/src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const logger = require("morgan");
+const cors = require('cors');
+const createError = require('http-errors')
+require('dotenv').config();
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+/* Routes */
+const indexRouter = require('./routes/indexRouter');
+
+
+/* Middlewares */
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+/* Routes */
+app.use('/api', indexRouter);
+
+// Ruta no encontrada
+app.use((req, res, next) => {
+    next(createError(404, 'Ruta no encontrada'))
+})
+
+
+app.listen(PORT, () => console.log(`Servidor levantado en el puerto ${PORT}\nhttp://localhost:${PORT}/api`));
--- server/src/routes/indexRouter.js
+++ server/src/routes/indexRouter.js
@@ -2,6 +2,13 @@ const express = require("express")
 const router = express.Router()
 const homeController = require("../controllers/homeController")
 
-router.get("/", homeController.home)
+router.get("/", (req, res) => {
+    res.status(200).json({
+        status: true,
+        msg: "Conexión exitosa"
+    })
+})
+
+
 
 module.exports = router;
