69e60683bccc15b12d25cb8f61077386cfa4f7e7
pedromax23 <pedromorgade1234@gmail.com>
2025-06-13 20:29:46 -0300

    Agrega validacion para crear y editar refugios

--- server/src/routes/admin/adminRefugiosRouter.js
+++ server/src/routes/admin/adminRefugiosRouter.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const refugioController = require('../../controllers/adminControllers/adminRefugiosController');
+const { singleUpload, handleMulterErrors } = require("../../middlewares/imageMiddleware")
+const refugioValidator = require('../../validations/refugioValidator');
 
-router.post('/', refugioController.create);
-router.put('/:id', refugioController.update);
+
+router.post('/', refugioValidator, singleUpload, handleMulterErrors, refugioController.create);
+router.put('/:id', refugioValidator, singleUpload, handleMulterErrors, refugioController.update);
 router.delete('/:id', refugioController.delete);
 
 module.exports = router;
