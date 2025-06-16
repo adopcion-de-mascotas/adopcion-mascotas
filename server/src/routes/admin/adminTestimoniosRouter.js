dac2e1a091447d143cd2768bf507ec6d682341b0
Brian Flores <brian.flores.ch@gmail.com>
2025-06-16 16:53:00 -0300

    Refactoriza codigo

--- server/src/routes/admin/adminTestimoniosRouter.js
+++ server/src/routes/admin/adminTestimoniosRouter.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const testimonioController = require("../../controllers/adminControllers/adminTestimoniosController");
-const upload = require("../../middlewares/multerTestimonios");
 const { handleMulterErrors } = require('../../middlewares/imageMiddleware')
 const testimoniosValidator = require("../../validations/testimoniosValidator");
 
 /* Agrega testimonio con imagen */
-router.post("/", testimoniosValidator, handleMulterErrors, upload.single('foto'), testimonioController.crearTestimonio);
+router.post("/", testimoniosValidator, handleMulterErrors, testimonioController.crearTestimonio);
 
 /* Edita datos de testimonio (con imagen opcional) */
-router.put("/:id", testimoniosValidator, handleMulterErrors, upload.single('foto'), testimonioController.actualizarTestimonio);
+router.put("/:id", testimoniosValidator, handleMulterErrors, testimonioController.actualizarTestimonio);
 
 /* Elimina datos de testimonio */
 router.delete("/:id", testimonioController.eliminarTestimonio);
