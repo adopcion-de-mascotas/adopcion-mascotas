06754d8fca4efca8a815f43bca5b95d8a0b5509a
Aubar48 <rociio.fressa@gmail.com>
2025-06-16 17:41:54 -0300

    fix name

--- frontend/src/components/mascotaFormEdit/MascotaFormEdit.jsx
+++ frontend/src/components/mascotaFormEdit/MascotaFormEdit.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useMascotaEdit } from "./useMascotaFormEdit";
+import { useMascotaFormEdit } from "./useMascotaFormEdit";
 
 export default function MascotaFormEdit() {
   const {
@@ -10,7 +10,7 @@ export default function MascotaFormEdit() {
     mensaje,
     error,
     cargando,
-  } = useMascotaEdit();
+  } = useMascotaFormEdit();
 
   const opcionesPersonalidad = [
     "Amigable",
