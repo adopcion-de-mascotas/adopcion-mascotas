99692af5730dc25808f54860d50b486c9b51f533
Brian Flores <brian.flores.ch@gmail.com>
2025-06-26 19:19:54 -0300

    Modifica seeders

--- server/src/database/seeders/20250525170847-demo-galeria-mascotas.js
+++ server/src/database/seeders/20250525170847-demo-galeria-mascotas.js
@@ -3,16 +3,16 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('galeria_mascotas', [
-      { foto: 'https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?auto=format&fit=crop&w=800&q=80', mascotaId: 1 },
-      { foto: 'https://images.unsplash.com/photo-1507146426996-ef05306b995a?auto=format&fit=crop&w=800&q=80', mascotaId: 1 },
-      { foto: 'https://images.unsplash.com/photo-1518717758536-85ae29035b6d?auto=format&fit=crop&w=800&q=80', mascotaId: 2 },
-      { foto: 'https://images.unsplash.com/photo-1546182990-dffeafbe841d?auto=format&fit=crop&w=800&q=80', mascotaId: 2 },
-      { foto: 'https://images.unsplash.com/photo-1558788353-f76d92427f16?auto=format&fit=crop&w=800&q=80', mascotaId: 3 },
-      { foto: 'https://images.unsplash.com/photo-1560807707-8cc77767d783?auto=format&fit=crop&w=800&q=80', mascotaId: 3 },
-      { foto: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?auto=format&fit=crop&w=800&q=80', mascotaId: 4 },
-      { foto: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?auto=format&fit=crop&w=800&q=80', mascotaId: 4 },
-      { foto: 'https://images.unsplash.com/photo-1546182990-dffeafbe841d?auto=format&fit=crop&w=800&q=80', mascotaId: 5 },
-      { foto: 'https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?auto=format&fit=crop&w=800&q=80', mascotaId: 5 }
+      { foto: 'https://media.istockphoto.com/id/2195402595/es/foto/golden-retriever-outdoors.webp?a=1&b=1&s=612x612&w=0&k=20&c=ShXlKX2L2-gePDrlyEWqVeJK0KAuiUgLJI24Av_t6y0=', mascotaId: 1 },
+      { foto: 'https://images.unsplash.com/photo-1625794084867-8ddd239946b1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGdvbGRlbiUyMHJldHJpZXZlcnxlbnwwfHwwfHx8MA%3D%3D', mascotaId: 1 },
+      { foto: 'https://media.istockphoto.com/id/1838410260/photo/cute-labrador-retriever-puppy-playing-with-ball-on-green-grass-in-park.jpg?s=612x612&w=0&k=20&c=3nJXtcvv7yTfvY1ZBFrwvFV5RkSnDmg7b5thQdZtWFE=', mascotaId: 2 },
+      { foto: 'https://media.istockphoto.com/id/1838409624/photo/cute-labrador-retriever-puppy-in-park-space-for-text.jpg?s=612x612&w=0&k=20&c=CtiqhAw-iI8bujSf_MOhh78ymefeixPXTGeO2nn1bEs=', mascotaId: 2 },
+      { foto: 'https://images.unsplash.com/photo-1613064504039-bb89104af499?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGdhdG8lMjBzaWFtZXN8ZW58MHx8MHx8fDA%3D', mascotaId: 3 },
+      { foto: 'https://images.unsplash.com/photo-1568309386325-ef86f13ac533?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Z2F0byUyMHNpYW1lc3xlbnwwfHwwfHx8MA%3D%3D', mascotaId: 3 },
+      { foto: 'https://media.istockphoto.com/id/2171627512/es/foto/bulldog-franc%C3%A9s-con-novia-en-el-fondo-prepar%C3%A1ndose-para-la-boda.webp?a=1&b=1&s=612x612&w=0&k=20&c=0TFIbZ1PTpYGx07hk1e9PEGB_1ORC_yxw77ZQYcFv1s=', mascotaId: 4 },
+      { foto: 'https://media.istockphoto.com/id/1199907866/photo/portrait-of-black-dog-looking-away.jpg?s=612x612&w=0&k=20&c=7-1d-b9IkS_7w5mqv_L3Bmajqi7mUtdHUthdkSWFag0=', mascotaId: 4 },
+      { foto: 'https://media.istockphoto.com/id/1176091130/photo/big-beautiful-cat-looking-forward-brown-hair-and-big-green-eyes-pet.jpg?s=612x612&w=0&k=20&c=e_LRdecIXz3Pbs0YoxNiumInhygeRwoD07IzX-li6Ik=', mascotaId: 5 },
+      { foto: 'https://images.unsplash.com/photo-1605725657590-b2cf0d31b1a5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFzdG9yJTIwYWxlbWFufGVufDB8fDB8fHww', mascotaId: 6 }
     ], {});
   },
 
--- server/src/database/seeders/20250525171005-demo-noticias.js
+++ server/src/database/seeders/20250525171005-demo-noticias.js
@@ -7,35 +7,35 @@ module.exports = {
         titulo: 'Nuevo refugio para mascotas en la ciudad',
         texto: 'Se inauguró un nuevo refugio para mascotas con capacidad para 100 animales. Contará con servicios veterinarios y espacio para adopciones.',
         fecha: new Date('2025-05-20'),
-        foto: 'https://example.com/images/refugio-inauguracion.jpg',
+        foto: 'https://media.istockphoto.com/id/1211334914/es/foto/vista-superior-de-dos-patas-de-perro-en-la-mano-del-propietario.webp?a=1&b=1&s=612x612&w=0&k=20&c=6D5DET5ARtjzSMNwj488zbix-7a3elFGYHSZKWVAUf8=',
         admin_id: 1
       },
       {
         titulo: 'Campaña de vacunación gratuita',
         texto: 'Durante todo el mes se realizará una campaña de vacunación gratuita para perros y gatos en diferentes barrios de la ciudad.',
         fecha: new Date('2025-05-15'),
-        foto: 'https://example.com/images/campana-vacunacion.jpg',
+        foto: 'https://plus.unsplash.com/premium_photo-1677166331454-b58e2b2355b7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDQ0fHx8ZW58MHx8fHx8',
         admin_id: 2
       },
       {
         titulo: 'Historias de adopción exitosas',
         texto: 'Compartimos varias historias emocionantes de adopciones recientes que cambiaron la vida de mascotas y familias.',
         fecha: new Date('2025-05-10'),
-        foto: 'https://example.com/images/adopcion-exitosa.jpg',
+        foto: 'https://plus.unsplash.com/premium_photo-1681843750412-b6abbce5630c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGN1aWRhZG8lMjBtYXNjb3Rhc3xlbnwwfHwwfHx8MA%3D%3D',
         admin_id: 3
       },
       {
         titulo: 'Voluntariado en el refugio',
         texto: 'Se abre la convocatoria para nuevos voluntarios que quieran ayudar en las tareas diarias del refugio y acompañar a las mascotas.',
         fecha: new Date('2025-05-05'),
-        foto: 'https://example.com/images/voluntariado.jpg',
+        foto: 'https://images.unsplash.com/photo-1559027615-cd4628902d4a?q=80&w=1474&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         admin_id: 1
       },
       {
         titulo: 'Talleres para cuidado de mascotas',
         texto: 'Próximamente se realizarán talleres gratuitos sobre cuidado y educación para mascotas, dictados por expertos veterinarios.',
         fecha: new Date('2025-04-30'),
-        foto: 'https://example.com/images/talleres-mascotas.jpg',
+        foto: 'https://images.unsplash.com/photo-1581888475780-27b6b0bc3690?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y3VpZGFkbyUyMG1hc2NvdGFzfGVufDB8fDB8fHww',
         admin_id: 2
       }
     ]);
